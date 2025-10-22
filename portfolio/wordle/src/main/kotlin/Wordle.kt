c1f2ac6e75d44c628f0b8df58f001eb770d86ad8
fy23oaia <fy23oaia@leeds.ac.uk>
2025-10-22 18:34:40 +0000

    m

--- portfolio/wordle/src/main/kotlin/Wordle.kt
+++ portfolio/wordle/src/main/kotlin/Wordle.kt
@@ -3,34 +3,44 @@ fun isValid(word: String): Boolean {
     return word.length == 5 && word.all { it.isLetter() }
 }
 
-fun readWordList(words.txt: String): MutableList<String> =
-    java.io.File(words.txt).readLines().toMutableList()
-
+fun readWordList(filename: String): MutableList<String> =
+    java.io.File(filename).readLines().toMutableList()
 
 fun pickRandomWord(words: MutableList<String>): String {
-    val word = word.random()
+    val word = words.random()
     words.remove(word)
     return word
 }
 
 fun obtainGuess(attempt: Int): String {
-    while(true){
-        println("Attempt $attemp:")
+    while (true) {
+        print("Attempt $attempt: ")   // stays on same line
         val guess = readln().trim().lowercase()
 
-        if (isValid(guess)){
+        if (isValid(guess)) {
             return guess
-        }
-        else{
-            println("Invalid guess, chose a 5 letter world.")
-        }
-            
-        }    
+        } else {
+            println("Invalid guess, choose a 5-letter word.")
         }
     }
 }
 
+fun evaluateGuess(guess: String, target: String): List<Int> {
+    val result = mutableListOf<Int>{
+        for (i in 0..4){
+            (if guess[i] == target[i]){
+                return.add(1)
+            }
+            else{
+                return.add(0)
+            }
+            
+        }
+    }
+    return result
 }
+
+
 
 
 
